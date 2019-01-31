4a7689107c12f657d6623a224cc40f593cda8fa0
Austin <joha0033@gmail.com>
2019-01-30 19:59:20 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +6,143 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+         <h1>Here's a attempt at a sidebar demo..</h1>
         </header>
+        <main>
+          <section className={'main-section'}>
+            <h1>A title will go here</h1>
+            <h4>
+              Hoop-LAH-D part 1
+            </h4>
+            <p>
+            Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. 
+            De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo 
+            vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. 
+            Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus 
+            resi dentevil vultus comedat cerebella viventium. Qui animated corpse, 
+            cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos 
+            flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for 
+            solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. 
+            Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+            </p><p>
+            Cum horribilem walking dead resurgere de crazed sepulcris creaturis, 
+            zombie sicut de grave feeding iride et serpens. Pestilentia, shaun of 
+            the dead scythe animated corpses ipsa screams. Pestilentia est plague 
+            haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. 
+            Aenean a dolor plan et terror soulless vulnerum contagium accedunt, 
+            mortui iam vivam unlife. Qui tardius moveri, brid eof reanimator sed 
+            in magna copia sint terribiles undeath legionis. Alii missing oculis 
+            aliorum sicut serpere crabs nostram. Putridi braindead odores kill and 
+            infect, aere implent left four dead.
+            </p>
+
+            <h4>
+              Hoop-LAH-D part 2 - oh, no...
+            </h4>
+            <p>
+            Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. 
+            De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo 
+            vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. 
+            Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus 
+            resi dentevil vultus comedat cerebella viventium. Qui animated corpse, 
+            cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos 
+            flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for 
+            solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. 
+            Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+            </p><p>
+            Cum horribilem walking dead resurgere de crazed sepulcris creaturis, 
+            zombie sicut de grave feeding iride et serpens. Pestilentia, shaun of 
+            the dead scythe animated corpses ipsa screams. Pestilentia est plague 
+            haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. 
+            Aenean a dolor plan et terror soulless vulnerum contagium accedunt, 
+            mortui iam vivam unlife. Qui tardius moveri, brid eof reanimator sed 
+            in magna copia sint terribiles undeath legionis. Alii missing oculis 
+            aliorum sicut serpere crabs nostram. Putridi braindead odores kill and 
+            infect, aere implent left four dead.
+            </p>
+          </section>
+          <section className={'sidebar'}>
+            <h2 className={'sidebar-title'}>TO DO BAR</h2>
+            <ul className={'content-list'}>
+            <div className={'vertical-line'}></div>
+              <li className={'list-item'}>
+                <div className={'bullet'}>
+                  
+                </div>
+                <a href={'/'} className={'section-title'}>
+                  thing to do
+                </a>
+                
+              </li>
+              <li className={'list-item'}>
+                <div className={'bullet'}>
+                  
+                </div>
+                <div className={'section-title'}>
+                  thing to do
+                </div>
+                
+              </li>
+              <li className={'list-item'}>
+
+                <div className={'bullet'}>
+                  
+                </div>
+
+                <div className={'section-title'}>
+                  thing to do
+                </div>
+                
+              </li>
+              <li className={'list-item'}>
+
+                <div className={'bullet'}>
+                  
+                </div>
+
+                <div className={'section-title'}>
+                  thing to do
+                </div>
+                
+              </li>
+              <li className={'list-item'}>
+
+                <div className={'bullet'}>
+                  
+                </div>
+
+                <div className={'section-title'}>
+                  thing to do
+                </div>
+                
+              </li>
+              <li className={'list-item'}>
+
+                <div className={'bullet'}>
+                  
+                </div>
+
+                <div className={'section-title'}>
+                  thing to do
+                </div>
+                
+              </li>
+              <li className={'list-item'}>
+
+                <div className={'bullet'}>
+                  
+                </div>
+
+                <div className={'section-title'}>
+                  thing to do
+                </div>
+                
+              </li>
+
+            </ul>
+          </section>
+          
+        </main>
       </div>
     );
   }
